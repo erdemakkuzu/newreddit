da10975a1c1d84343351dca0b6d5d1b697ac5fc9
Erdem <m.erdemakkuzu@gmail.com>
2019-05-11 01:53:10 +0300

    Adding a welcome message.

--- src/main/java/com/erdem/newreddit/NewredditApplication.java
+++ src/main/java/com/erdem/newreddit/NewredditApplication.java
@@ -8,6 +8,7 @@ public class NewredditApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NewredditApplication.class, args);
+        System.out.println("Welcome to Springit!");
     }
 
 }
